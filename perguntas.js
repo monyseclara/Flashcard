40bbba59600db122ac22580c1079fbd9f9fa11de
Monyse Clara de Lima <137203508+monyseclara@users.noreply.github.com>
2024-10-21 07:56:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a capital da França?',
+    'Paris'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem é conhecido como o "Rei do Pop"?',
+    'Michael Jackson'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem inventou a lâmpada elétrica?',
+    'Thomas Edison'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o idioma mais falado no mundo?',
+    'Mandarim'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Em que continente está o Egito?',
+    'África'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o elemento químico com símbolo O?',
+    'Oxigênio'
+)
